19f7e6f539fb0a3234182fe6a618c9abcba805d9
vlisitzin <vlisitzin@C11KWT2.corp.proofpoint.com>
2019-09-24 10:18:12 +0300

    Edited the models with basic variables

--- couponproject/src/main/java/com/jbp/couponproject/models/Coupon.java
+++ couponproject/src/main/java/com/jbp/couponproject/models/Coupon.java
@@ -1,5 +1,20 @@
 package com.jbp.couponproject.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class Coupon {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	@NotNull
+	private String title;
+	@NotNull
+	private double price;
 
 }
--- couponproject/src/main/java/com/jbp/couponproject/models/User.java
+++ couponproject/src/main/java/com/jbp/couponproject/models/User.java
@@ -1,5 +1,20 @@
 package com.jbp.couponproject.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class User {
 
+	@Id
+	@NotNull
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	@NotNull
+	private String name;
+	@NotNull
+	private String password;
 }

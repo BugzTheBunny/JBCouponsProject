a8eb5a0d500c92eab7bb653f9d48ebdd51a20cf5
vlisitzin <vlisitzin@C11KWT2.corp.proofpoint.com>
2019-09-24 10:03:12 +0300

    First Commit - Empty project.

---
+++ couponproject/src/main/java/com/jbp/couponproject/CouponprojectApplication.java
@@ -0,0 +1,13 @@
+package com.jbp.couponproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponprojectApplication.class, args);
+	}
+
+}

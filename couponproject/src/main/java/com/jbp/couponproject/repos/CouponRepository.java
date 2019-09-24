809fc5e5c8d5bb1077113784eef11b27b4c20ea6
vlisitzin <vlisitzin@C11KWT2.corp.proofpoint.com>
2019-09-24 10:26:47 +0300

    Fixes

--- couponproject/src/main/java/com/jbp/couponproject/repos/CouponRepository.java
+++ couponproject/src/main/java/com/jbp/couponproject/repos/CouponRepository.java
@@ -1,6 +1,8 @@
 package com.jbp.couponproject.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.RepositoryDefinition;
+import org.springframework.stereotype.Repository;
 
 import com.jbp.couponproject.models.Coupon;
 

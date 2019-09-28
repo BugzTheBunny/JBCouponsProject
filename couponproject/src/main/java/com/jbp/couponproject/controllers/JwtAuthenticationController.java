241d8071788283f62c33a5ec2632ab01263ad828
slava <urigami2010@gmail.com>
2019-09-28 13:37:10 +0300

    Explanation about how its working.

--- couponproject/src/main/java/com/jbp/couponproject/controllers/JwtAuthenticationController.java
+++ couponproject/src/main/java/com/jbp/couponproject/controllers/JwtAuthenticationController.java
@@ -1,9 +1,6 @@
 package com.jbp.couponproject.controllers;
 
-import javax.management.relation.Role;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -18,15 +15,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jbp.couponproject.config.JwtTokenUtil;
-import com.jbp.couponproject.enums.Roles;
 import com.jbp.couponproject.models.JwtRequest;
 import com.jbp.couponproject.models.JwtResponse;
 import com.jbp.couponproject.models.UserDTO;
 import com.jbp.couponproject.repos.UserModelRepository;
 import com.jbp.couponproject.service.JwtUserDetailsService;
-
-import javassist.expr.NewArray;
-import springfox.documentation.service.ResponseMessage;
 
 @RestController
 @CrossOrigin
@@ -37,13 +30,16 @@ public class JwtAuthenticationController {
 
 	@Autowired
 	private JwtTokenUtil jwtTokenUtil;
-	
+
 	@Autowired
 	UserModelRepository userModelRepository;
 
 	@Autowired
 	private JwtUserDetailsService userDetailsService;
-
+	
+	/*
+	 * This method is used for the Login, checks the credentials and sets them.
+	 */
 	@RequestMapping(value = "/authenticate", method = RequestMethod.POST)
 	public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
 
@@ -51,12 +47,23 @@ public class JwtAuthenticationController {
 
 		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
 
+		/*
+		 *  sets the user Token + the Role.
+		 */
 		final String token = jwtTokenUtil.generateToken(userDetails);
-		final String role = userModelRepository.findByUsername(authenticationRequest.getUsername()).getRoles().toString();
+		final String role = userModelRepository.findByUsername(authenticationRequest.getUsername()).getRoles()
+				.toString();
 
-		return ResponseEntity.ok(new JwtResponse(token,role));
+		/*
+		 *  Returns the Token + the Role of the user.
+		 */
+		return ResponseEntity.ok(new JwtResponse(token, role));
 	}
 
+	/*
+	 * The post request for the registration, takes a user infetrface that includes
+	 * Username, Password and the Role.
+	 */
 	@PostMapping(value = "/register")
 	public ResponseEntity<?> saveUser(@RequestBody UserDTO user) throws Exception {
 		System.out.println("New account has been registerd" + user.toString());

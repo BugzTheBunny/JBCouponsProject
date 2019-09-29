f2fad5832e7208dd9657ad5e7e310c4aeab76355
slava <urigami2010@gmail.com>
2019-09-29 17:14:41 +0300

    Added user delete function

--- couponproject/src/main/java/com/jbp/couponproject/controllers/UsersController.java
+++ couponproject/src/main/java/com/jbp/couponproject/controllers/UsersController.java
@@ -2,21 +2,28 @@ package com.jbp.couponproject.controllers;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jbp.couponproject.enums.Roles;
+import com.jbp.couponproject.models.Coupon;
 import com.jbp.couponproject.models.UserModel;
 import com.jbp.couponproject.repos.CouponRepository;
 import com.jbp.couponproject.repos.UserModelRepository;
 
+import javassist.expr.NewArray;
+
 @CrossOrigin
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/user")
 public class UsersController {
 
 	@Autowired
@@ -42,6 +49,21 @@ public class UsersController {
 		List<UserModel> users = userRepository.findByRole(Roles.MANAGER.toString());
 		return users;
 
+	}
+
+	@Transactional
+	@DeleteMapping(path = { "/del/{id}" })
+	public void delete(Authentication authentication, @PathVariable("id") long id) {
+		System.out.println("Delete was called");
+		// Will work only for ADMIN
+		if (userRepository.findByUsername(authentication.getName()).getRoles().toString().equals("ADMIN")) {
+			if (userRepository.findById(id) != null) {
+				UserModel temp = userRepository.findById(id);
+				userRepository.findById(id).setCoupons(null);
+				userRepository.save(temp);
+				userRepository.delete(temp);
+			}
+		}
 	}
 
 }
--- couponproject/src/main/java/com/jbp/couponproject/repos/UserModelRepository.java
+++ couponproject/src/main/java/com/jbp/couponproject/repos/UserModelRepository.java
@@ -18,4 +18,6 @@ public interface UserModelRepository extends JpaRepository<UserModel, Integer> {
 	List<Coupon> findAllCouponsByUsername(String username);
 
 	List<UserModel> findByRole(String string);
+
+	UserModel findById(long id);
 }
